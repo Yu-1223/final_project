13323259ade3c8638524b3a4dda6b2603938b8d4
Yu-1223 <92169580+Yu-1223@users.noreply.github.com>
2022-06-06 15:36:05 +0800

    Update card_info.c

--- card_info.c
+++ card_info.c
@@ -13,28 +13,6 @@ typedef struct _sCard sCard;
 int32_t card_info(FILE *fp , sCard *info , int32_t count);
 int32_t card_struct_init(sCard *info);
 
-int main()
-{
-    FILE *cardFile;
-    if( (cardFile = fopen("card.txt" , "r")) == NULL )
-    {
-        printf("cardFile can't be open!\n");
-    }
-
-    sCard info;
-    int32_t count = 0;
-
-    printf("count: ");
-    scanf("%d",&count);
-
-    card_info(cardFile , &info , count);
-    printf("num: %d\n",info.num);
-    for(int32_t i = 0 ; i<25 ; i++)
-    {
-        printf("suit: %d\n",info.suit[i]);
-    }
-}
-
 int32_t card_info(FILE *cardFile , sCard *info , int32_t count)
 {
     card_struct_init(info);
